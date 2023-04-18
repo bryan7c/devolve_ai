5ff47e1e603cb83f48f2a016711dbb4bcac68a0c
Bryan Marvila <bryan7c@gmail.com>
2023-04-17 22:15:48 -0300

    feat create route function

--- src/components/map/AutoCompleteLocation.jsx
+++ src/components/map/AutoCompleteLocation.jsx
@@ -2,30 +2,93 @@ import React, { useState } from "react";
 import { Autocomplete, useLoadScript } from "@react-google-maps/api";
 import { InputBase } from "@mui/material";
 
-function AutoCompleteLocation({placeholder}) {
-  const [searchResult, setSearchResult] = useState("Result: none");
-
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: "AIzaSyDEI0-FS-iJl25mu23dSfFLzodOZZ4Vr3k",
-    libraries: ["places"]
-  });
+function AutoCompleteLocation({
+  placeholder,
+  onPlaceChanged,
+  isLoaded,
+  onResult,
+}) {
+  const [searchResult, setSearchResult] = useState(null);
 
   function onLoad(autocomplete) {
     setSearchResult(autocomplete);
   }
 
-  function onPlaceChanged() {
+  function handlePlaceChanged() {
     if (searchResult != null) {
       const place = searchResult.getPlace();
-      const name = place.name;
-      const status = place.business_status;
-      const formattedAddress = place.formatted_address;
-      // console.log(place);
-      console.log(`Name: ${name}`);
-      console.log(`Business Status: ${status}`);
-      console.log(`Formatted Address: ${formattedAddress}`);
-    } else {
-      alert("Please enter text");
+      onPlaceChanged(place);
+    }
+  }
+
+  async function handlePlaceChanged() {
+    if (searchResult != null) {
+      const place = searchResult.getPlace();
+      if (place.place_id) {
+        const coord = await getCoordinates(place);
+        onPlaceChanged(coord);
+      }
+    }
+  }
+
+  function getCoordinates(place) {
+    return new Promise((resolve, reject) => {
+      const service = new window.google.maps.places.PlacesService(
+        document.createElement("div")
+      );
+      service.getDetails({ placeId: place.place_id }, (result, status) => {
+        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+          const coord = result.geometry.location.toJSON();
+          resolve(coord);
+        } else {
+          reject(
+            new Error(`Failed to get coordinates for place ID ${place_id}`)
+          );
+        }
+      });
+    });
+  }
+
+  function handleSearchKeyDown(event) {
+    if (event.key === "Enter") {
+      const query = event.target.value;
+      const service = new window.google.maps.places.AutocompleteService();
+      service.getPlacePredictions({ input: query }, (predictions) => {
+        const placesService = new window.google.maps.places.PlacesService(
+          document.createElement("div")
+        );
+        const promises = predictions.map((prediction) => {
+          return new Promise((resolve, reject) => {
+            placesService.getDetails(
+              { placeId: prediction.place_id },
+              (placeResult, status) => {
+                if (
+                  status === window.google.maps.places.PlacesServiceStatus.OK
+                ) {
+                  const {lat, lng} = placeResult.geometry.location.toJSON();
+                  resolve({
+                    lat,
+                    lng,
+                    description: prediction.description,
+                    name: placeResult.name,
+                    photos: placeResult.photos.map(photo => photo.getUrl()),
+                    place_id: placeResult.place_id,
+                  });
+                } else {
+                  reject(status);
+                }
+              }
+            );
+          });
+        });
+        Promise.all(promises)
+          .then((results) => {
+            onResult(results);
+          })
+          .catch((status) => {
+            console.log(status);
+          });
+      });
     }
   }
 
@@ -34,9 +97,9 @@ function AutoCompleteLocation({placeholder}) {
   }
 
   return (
-        <Autocomplete onPlaceChanged={onPlaceChanged} onLoad={onLoad}>
-          <InputBase placeholder={placeholder} />
-        </Autocomplete>
+    <Autocomplete onPlaceChanged={handlePlaceChanged} onLoad={onLoad}>
+      <InputBase placeholder={placeholder} onKeyDown={handleSearchKeyDown} />
+    </Autocomplete>
   );
 }
 
